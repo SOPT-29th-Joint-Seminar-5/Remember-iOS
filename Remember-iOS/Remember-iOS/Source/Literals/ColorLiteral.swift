dd65abf5ffd6a1536d2722b3c3edcf135d7d835a
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-13 16:02:02 +0900

    ➕[ADD] 기본 Color 추가(#1)

---
+++ Remember-iOS/Remember-iOS/Source/Literals/ColorLiteral.swift
@@ -0,0 +1,45 @@
+//
+//  ColorLiteral.swift
+//  Remember-iOS
+//
+//  Created by SHIN YOON AH on 2021/11/13.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var main1: UIColor {
+        return UIColor(red: 234/255, green: 81/255, blue: 40/255, alpha: 1)
+    }
+    
+    @nonobjc class var main2: UIColor {
+        return UIColor(red: 1, green: 180/255, blue: 110/255, alpha: 1)
+    }
+    
+    @nonobjc class var main3: UIColor {
+        return UIColor(red: 1, green: 236/255, blue: 219/255, alpha: 1)
+    }
+    
+    @nonobjc class var sub1: UIColor {
+        return UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+    }
+
+    @nonobjc class var gray1: UIColor {
+        return UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    }
+    
+    @nonobjc class var gray2: UIColor {
+        return UIColor(red: 204/255, green: 204/255, blue: 204/255, alpha: 1)
+    }
+    
+    @nonobjc class var gray3: UIColor {
+        return UIColor(red: 174/255, green: 174/255, blue: 174/255, alpha: 1)
+    }
+    
+    @nonobjc class var gray4: UIColor {
+        return UIColor(red: 140/255, green: 140/255, blue: 140/255, alpha: 1)
+    }
+    
+}
+
