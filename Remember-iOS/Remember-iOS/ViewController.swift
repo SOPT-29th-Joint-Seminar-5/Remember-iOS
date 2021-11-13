d9b25ac2a243dc0c257ba97dc1ab64808b0ac1fd
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-13 14:25:35 +0900

    🎉 inital setting

---
+++ Remember-iOS/Remember-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remember-iOS
+//
+//  Created by SHIN YOON AH on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
