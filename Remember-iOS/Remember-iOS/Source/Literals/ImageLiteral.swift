ae8e1468ab4eb3d89d468fd7b20d044bc191a472
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-13 16:23:08 +0900

    🍱[ADD] Asset 추가(#1)

---
+++ Remember-iOS/Remember-iOS/Source/Literals/ImageLiteral.swift
@@ -0,0 +1,54 @@
+//
+//  ImageLiteral.swift
+//  Remember-iOS
+//
+//  Created by SHIN YOON AH on 2021/11/13.
+//
+
+import UIKit
+
+public enum ImageLiterals {
+
+    public static var btnPhoto: UIImage { .load(name: "btnPhoto")}
+    public static var btnShare: UIImage { .load(name: "btnShare") }
+    public static var icbtComment: UIImage { .load(name: "icbtComment") }
+    public static var icbtCommentList: UIImage { .load(name: "icbtCommentList") }
+    public static var icbtDropdown: UIImage { .load(name: "icbtDropdown") }
+    public static var icbtLike: UIImage { .load(name: "icbtLike") }
+    public static var icbtLikeHeart: UIImage { .load(name: "icbtLikeHeart") }
+    public static var icbtRightChervon: UIImage { .load(name: "icbtRightChervon") }
+    public static var iconBack: UIImage { .load(name: "iconBack") }
+    public static var iconMessagecircleSeleted: UIImage { .load(name: "iconMessagecircleSeleted") }
+    public static var iconMessagecircleUnseleted: UIImage { .load(name: "iconMessagecircleUnseleted") }
+    public static var iconMoreHorizontal: UIImage { .load(name: "iconMoreHorizontal") }
+    public static var iconMoreVertical: UIImage { .load(name: "iconMoreVertical") }
+    public static var iconNameCardSelected: UIImage { .load(name: "iconNameCardSelected") }
+    public static var iconNameCardUnselected: UIImage { .load(name: "iconNameCardUnselected") }
+    public static var iconUserSelected: UIImage { .load(name: "iconUserSelected") }
+    public static var iconUserUnseleted: UIImage { .load(name: "iconUserUnseleted") }
+    public static var icUser: UIImage { .load(name: "icUser") }
+    public static var nicknameregisterActive: UIImage { .load(name: "nicknameregister_active") }
+    public static var nicknameregisterInactive: UIImage { .load(name: "nicknameregister_inactive") }
+    public static var surfaceAd: UIImage { .load(name: "surface_ad") }
+    
+}
+
+extension UIImage {
+    fileprivate static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            assert(false, "\(name) 이미지 로드 실패")
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+    internal func resize(to length: CGFloat) -> UIImage {
+        let newSize = CGSize(width: length, height: length)
+        let image = UIGraphicsImageRenderer(size: newSize).image { _ in
+            draw(in: CGRect(origin: .zero, size: newSize))
+        }
+            
+        return image
+    }
+}
