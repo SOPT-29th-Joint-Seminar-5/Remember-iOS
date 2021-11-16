eccad40221f59591de7b5924327947b28c4ebf26
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-16 18:40:23 +0900

    ➕[ADD] Xib, Custom View 추가(#9)

---
+++ Remember-iOS/Remember-iOS/Source/Components/CommentSection.swift
@@ -0,0 +1,25 @@
+//
+//  CommentSection.swift
+//  Remember-iOS
+//
+//  Created by SHIN YOON AH on 2021/11/16.
+//
+
+import UIKit
+
+class CommentSection: UIView {
+
+    // MARK: - @IBOutlet
+    
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
--- Remember-iOS/Remember-iOS/Source/ViewControllers/DetailPostVC.swift
+++ Remember-iOS/Remember-iOS/Source/ViewControllers/DetailPostVC.swift
@@ -16,11 +16,11 @@ class DetailPostVC: DataTableViewController {
     
     private lazy var navigationBar = NavigationBar(root: self)
     
-    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
         render()
     }
     
@@ -32,6 +32,17 @@ class DetailPostVC: DataTableViewController {
                 $0.top.equalTo(self.view.safeAreaLayoutGuide)
                 $0.width.equalTo(UIScreen.main.bounds.size.width)
                 $0.height.equalTo(48)
+            }
+        }
+        
+        guard let commentView = Bundle.main.loadNibNamed("CommentSection", owner: self, options: nil)?.first as? CommentSection else { return }
+        commentView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 132)
+        view.add(commentView){
+            $0.snp.makeConstraints {
+                $0.leading.trailing.equalToSuperview()
+                $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
+                $0.width.equalTo(UIScreen.main.bounds.size.width)
+                $0.height.equalTo(132)
             }
         }
     }
