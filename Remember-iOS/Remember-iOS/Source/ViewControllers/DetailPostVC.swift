e51f966978a05866d12f67f7cb3452b862dd9931
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-16 19:05:26 +0900

    ➕[ADD] Comment뷰 전체 구현 완료(#9)

--- Remember-iOS/Remember-iOS/Source/ViewControllers/DetailPostVC.swift
+++ Remember-iOS/Remember-iOS/Source/ViewControllers/DetailPostVC.swift
@@ -16,11 +16,20 @@ class DetailPostVC: DataTableViewController {
     
     private lazy var navigationBar = NavigationBar(root: self)
     
+    // MARK: - UI
+    
+    private var commentSection: CommentSection?
+    
+    // MARK: - Properties
+    
+    private let placeholder = "닉네임님 생각은 어떠세요?"
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
+        setupTextView()
         render()
     }
     
@@ -35,8 +44,7 @@ class DetailPostVC: DataTableViewController {
             }
         }
         
-        guard let commentView = Bundle.main.loadNibNamed("CommentSection", owner: self, options: nil)?.first as? CommentSection else { return }
-        commentView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 132)
+        guard let commentView = commentSection else { return }
         view.add(commentView){
             $0.snp.makeConstraints {
                 $0.leading.trailing.equalToSuperview()
@@ -45,5 +53,40 @@ class DetailPostVC: DataTableViewController {
                 $0.height.equalTo(132)
             }
         }
+    }
+    
+    private func setupTextView() {
+        commentSection = Bundle.main.loadNibNamed("CommentSection", owner: self, options: nil)?.first as? CommentSection
+        commentSection?.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 132)
+        commentSection?.makeShadow(.gray2, 0.2, CGSize(width: 0, height: -5), 3)
+        
+        commentSection?.commentTextView.delegate = self
+        commentSection?.commentTextView.font = .systemFont(ofSize: 16)
+        commentSection?.commentTextView.text = placeholder
+        commentSection?.commentTextView.textColor = .gray2
+    }
+}
+
+extension DetailPostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == placeholder {
+            textView.text.removeAll()
+            textView.textColor = .black
+        }
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
+            self.commentSection?.transform = CGAffineTransform(translationX: 0, y: -300)
+        }, completion: nil)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == placeholder || !textView.hasText {
+            textView.text = placeholder
+            textView.textColor = .gray2
+        }
+        
+        UIView.animate(withDuration: 0.0, delay: 0, options: .curveEaseInOut, animations: {
+            self.commentSection?.transform = .identity
+        }, completion: nil)
     }
 }
